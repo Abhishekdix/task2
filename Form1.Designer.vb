6b328b85b7b63e55eacb6df6832bfe9f559be322
Abhishekdix <81672558+Abhishekdix@users.noreply.github.com>
2021-08-20 00:10:36 +0530

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,319 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.DTP1 = New System.Windows.Forms.DateTimePicker()
+        Me.DTP2 = New System.Windows.Forms.DateTimePicker()
+        Me.lblfromdate = New System.Windows.Forms.Label()
+        Me.lbltodate = New System.Windows.Forms.Label()
+        Me.btnGetdata = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DTP1
+        '
+        Me.DTP1.Location = New System.Drawing.Point(12, 45)
+        Me.DTP1.Name = "DTP1"
+        Me.DTP1.Size = New System.Drawing.Size(154, 27)
+        Me.DTP1.TabIndex = 0
+        '
+        'DTP2
+        '
+        Me.DTP2.Location = New System.Drawing.Point(172, 45)
+        Me.DTP2.Name = "DTP2"
+        Me.DTP2.Size = New System.Drawing.Size(153, 27)
+        Me.DTP2.TabIndex = 1
+        '
+        'lblfromdate
+        '
+        Me.lblfromdate.AutoSize = True
+        Me.lblfromdate.Location = New System.Drawing.Point(12, 22)
+        Me.lblfromdate.Name = "lblfromdate"
+        Me.lblfromdate.Size = New System.Drawing.Size(79, 20)
+        Me.lblfromdate.TabIndex = 2
+        Me.lblfromdate.Text = "From Date"
+        '
+        'lbltodate
+        '
+        Me.lbltodate.AutoSize = True
+        Me.lbltodate.Location = New System.Drawing.Point(183, 22)
+        Me.lbltodate.Name = "lbltodate"
+        Me.lbltodate.Size = New System.Drawing.Size(61, 20)
+        Me.lbltodate.TabIndex = 3
+        Me.lbltodate.Text = "To Date"
+        '
+        'btnGetdata
+        '
+        Me.btnGetdata.BackgroundImage = CType(resources.GetObject("btnGetdata.BackgroundImage"), System.Drawing.Image)
+        Me.btnGetdata.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnGetdata.Location = New System.Drawing.Point(323, 80)
+        Me.btnGetdata.Name = "btnGetdata"
+        Me.btnGetdata.Size = New System.Drawing.Size(130, 68)
+        Me.btnGetdata.TabIndex = 4
+        Me.btnGetdata.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.BackgroundImage = CType(resources.GetObject("btnReset.BackgroundImage"), System.Drawing.Image)
+        Me.btnReset.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.btnReset.Location = New System.Drawing.Point(459, 80)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(135, 68)
+        Me.btnReset.TabIndex = 5
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.BackgroundImage = CType(resources.GetObject("Button3.BackgroundImage"), System.Drawing.Image)
+        Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button3.Location = New System.Drawing.Point(600, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 136)
+        Me.Button3.TabIndex = 6
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.BackgroundImage = CType(resources.GetObject("Button4.BackgroundImage"), System.Drawing.Image)
+        Me.Button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button4.Location = New System.Drawing.Point(706, 13)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(99, 136)
+        Me.Button4.TabIndex = 7
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(811, 33)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(162, 27)
+        Me.TextBox1.TabIndex = 8
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(811, 103)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(162, 27)
+        Me.TextBox2.TabIndex = 9
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(1012, 33)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(168, 27)
+        Me.TextBox3.TabIndex = 10
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(1012, 103)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(168, 27)
+        Me.TextBox4.TabIndex = 11
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(1208, 33)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(161, 27)
+        Me.TextBox5.TabIndex = 12
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(1208, 103)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(161, 27)
+        Me.TextBox6.TabIndex = 13
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(811, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 20)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Search by Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(811, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(170, 20)
+        Me.Label2.TabIndex = 15
+        Me.Label2.Text = "Search by whatsApp No."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(1012, 12)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(130, 20)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "Search By Address"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(1012, 80)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(118, 20)
+        Me.Label4.TabIndex = 17
+        Me.Label4.Text = "Search by State :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(1208, 12)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(102, 20)
+        Me.Label5.TabIndex = 18
+        Me.Label5.Text = "Search by City"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(1208, 80)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(136, 20)
+        Me.Label6.TabIndex = 19
+        Me.Label6.Text = "Search by Zip code"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.BackColor = System.Drawing.Color.PaleGoldenrod
+        Me.TextBox7.Location = New System.Drawing.Point(325, 13)
+        Me.TextBox7.Multiline = True
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox7.Size = New System.Drawing.Size(269, 61)
+        Me.TextBox7.TabIndex = 20
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 142)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(93, 24)
+        Me.CheckBox1.TabIndex = 21
+        Me.CheckBox1.Text = "Select All"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 172)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 29
+        Me.DataGridView1.Size = New System.Drawing.Size(1381, 555)
+        Me.DataGridView1.TabIndex = 22
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.RoyalBlue
+        Me.ClientSize = New System.Drawing.Size(1381, 700)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnGetdata)
+        Me.Controls.Add(Me.lbltodate)
+        Me.Controls.Add(Me.lblfromdate)
+        Me.Controls.Add(Me.DTP2)
+        Me.Controls.Add(Me.DTP1)
+        Me.Name = "Form1"
+        Me.Text = "Bulk WhatsApp Messenger to Debt customer"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DTP1 As DateTimePicker
+    Friend WithEvents DTP2 As DateTimePicker
+    Friend WithEvents lblfromdate As Label
+    Friend WithEvents lbltodate As Label
+    Friend WithEvents btnGetdata As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ Form1.vb
@@ -0,0 +1,59 @@
+﻿
+
+Imports Excel = Microsoft.Office.Interop.Excel
+
+Imports System.Data.OleDb
+Imports OleDbDataAdapter
+
+Public Class Form1
+    Private Sub DTP1_ValueChanged(sender As Object, e As EventArgs) Handles DTP1.ValueChanged
+
+    End Sub
+
+    Private Sub lblfromdate_Click(sender As Object, e As EventArgs) Handles lblfromdate.Click
+
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub btnGetdata_Click(sender As Object, e As EventArgs) Handles btnGetdata.Click
+        Try
+
+            Dim MyConnection As System.Data.OleDb.OleDbConnection
+            Dim dataSet As System.Data.DataSet
+            Dim MyCommand As System.Data.OleDb.OleDbDataAdapter
+            Dim path As String = "D:\task22.xlsx"
+
+            MyConnection = New System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=Excel 12.0;")
+            MyCommand = New System.Data.OleDb.OleDbDataAdapter("select * from [Sheet1$]", MyConnection)
+
+            dataSet = New System.Data.DataSet
+            MyCommand.Fill(dataSet)
+            DataGridView1.DataSource = dataSet.Tables(0)
+
+            MyConnection.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        End Try
+
+
+    End Sub
+
+    Private Function OleDbDataAdapter(v As String, myConnection As Excel.OLEDBConnection) As OleDbDataAdapter
+        Throw New NotImplementedException()
+    End Function
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
+End Class
